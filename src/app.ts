4945c4121f3ad8b8be12910e79fc1964f380749f
Mayank-SDE <mayankchoudhary6055@gmail.com>
2024-06-19 10:53:32 +0530

    set the session configuration secure to true

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
