b536df3defe2f48d39d35ec5d9e28cc63b93aefa
Mayank-SDE <mayankchoudhary6055@gmail.com>
2024-06-23 17:05:49 +0530

    testing

--- src/app.ts
+++ src/app.ts
@@ -51,7 +51,7 @@ export const stripe = new Stripe(STRIPE_KEY);
 export const nodeCache = new NodeCache();
 
 const app = express();
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 
 // Session configuration
 app.use(
